0be737932a73a4a46ffbb8a2ac89ff6e425afded
Alexey Gorbunov <a.gorbunov4221@gmail.com>
2025-07-04 20:07:51 +0700

    fix for previous commit thanks to ci

--- app/settings.py
+++ app/settings.py
@@ -2,7 +2,6 @@ from pydantic_settings import BaseSettings
 
 class AppSettings(BaseSettings):
     DB_NAME: str
-    SERVICE_NAME: str
     SERVICE_HOST: str
     SERVICE_PORT: int
     SECRET_TOKEN: str
